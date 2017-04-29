b788a88f7b4b481b5038f06f52d491b1f7d47d44
Mike Mason <mike-git@ca.mrm.land>
2017-04-29 15:33:08 -0500

    Updated __init__ to allow for creation of new objects without initial values Moved render from ApacheParser to ApacheSection for easier accessibility Created configBuilder to allow for easy config building (Somewhat works)

--- ApacheConfig.py
+++ ApacheConfig.py
@@ -1,6 +1,6 @@
 import re, shlex
 
-__all__ = ['ApacheParser', 'ApacheRoot', 'ApacheSection', 'ApacheStatement', 'ApacheComment', 'ApacheEmptyLine', 'ApacheItem', 'ApacheItemList']
+__all__ = ['ApacheParser', 'ApacheRoot', 'ApacheSection', 'ApacheStatement', 'ApacheComment', 'ApacheEmptyLine', 'ApacheItem', 'ApacheItemList', 'configBuilder']
 
 if not hasattr(shlex, 'quote'):
 	if not hasattr(re, 'ASCII'):
@@ -28,12 +28,6 @@ match_line_endings = re.compile(br"\r?\n")
 class ApacheParseException(Exception): pass
 
 class ApacheItemList(list):
-
-	def insertBefore(self, n, o):
-		for j in range(len(self)):
-			if self[j] is o:
-				self.insert(j, n)
-				return self
 
 	def find(self, name, *arguments):
 		for i in self:
@@ -69,7 +63,7 @@ class ApacheItemList(list):
 		return self
 
 class ApacheItem(object):
-	def __init__(self, line, parent, file, index):
+	def __init__(self, line = None, parent = None, file = None, index = None):
 		self.line = line
 		self.parent = parent
 		self.file = file
@@ -82,18 +76,19 @@ class ApacheItem(object):
 		return self
 
 	def __str__(self):
-		return self.line.strip().decode('utf-8')
+		return self.line.strip().decode('utf-8') if self.line else ''
 
 	def __repr__(self):
-		return '<%s @ Line %d>' % (self.__class__.__name__, self.index)
+		return '<%s @ Line %d>' % (self.__class__.__name__, self.index if self.index != None else -1)
 
 class ApacheEmptyLine(ApacheItem): pass
 
 class ApacheComment(ApacheItem):
-	def __init__(self, line, parent, file, index):
+	def __init__(self, line = None, parent = None, file = None, index = None):
 		super(self.__class__, self).__init__(line, parent, file, index)
 		self.comment = None
-		self.parse()
+		if line:
+			self.parse()
 
 	def parse(self):
 		parts = re.search(match_comment, self.line)
@@ -113,11 +108,12 @@ class ApacheComment(ApacheItem):
 		return '# ' + self.comment.decode('utf-8')
 
 class ApacheStatement(ApacheItem):
-	def __init__(self, line, parent, file, index):
+	def __init__(self, line = None, parent = None, file = None, index = None):
 		super(self.__class__, self).__init__(line, parent, file, index)
 		self.module = None
 		self.arguments = []
-		self.parse()
+		if line:
+			self.parse()
 
 	def parse(self):
 		parts = shlex.split(self.line.decode('utf-8'))
@@ -132,7 +128,10 @@ class ApacheStatement(ApacheItem):
 
 	def update(self, *values, **kwargs):
 		replace_all = kwargs.pop('replace_all', False)
-		self.arguments = values if replace_all else (list(values) + self.arguments[len(values):])
+		if not self.arguments or replace_all:
+			self.arguments = values
+		elif self.arguments:
+			self.arguments = list(values) + self.arguments[len(values):]
 		return self
 
 	def __str__(self):
@@ -140,15 +139,16 @@ class ApacheStatement(ApacheItem):
 		return '%s %s' % (self.module, ' '.join(quoted_args))
 
 	def __repr__(self):
-		return "<%s '%s' @ Line %d>" % (self.__class__.__name__, self.module, self.index)
+		return "<%s '%s' @ Line %d>" % (self.__class__.__name__, self.module, self.index if self.index != None else -1)
 
 class ApacheSection(ApacheItem):
-	def __init__(self, line, parent, file, index):
+	def __init__(self, line = None, parent = None, file = None, index = None):
 		super(ApacheSection, self).__init__(line, parent, file, index)
 		self.name = None
 		self.arguments = None
 		self.children = ApacheItemList()
-		self.parse()
+		if line:
+			self.parse()
 
 	def parse(self):
 		if self.line:
@@ -165,7 +165,10 @@ class ApacheSection(ApacheItem):
 
 	def update(self, *values, **kwargs):
 		replace_all = kwargs.pop('replace_all', False)
-		self.arguments = values if replace_all else (list(values) + self.arguments[len(values):])
+		if not self.arguments or replace_all:
+			self.arguments = values
+		elif self.arguments:
+			self.arguments = list(values) + self.arguments[len(values):]
 		return self
 
 	def find(self, name, *arguments):
@@ -176,23 +179,80 @@ class ApacheSection(ApacheItem):
 
 	def appendChild(self, i):
 		self.children.append(i)
+		i.parent = self
 
 	def insertBefore(self, n, o):
-		self.children.insertBefore(n, o)
+		for j in range(len(self.children)):
+			if self.children[j] is o:
+				self.children.insert(j, n)
+				n.parent = self
+				return self
+
+	def renderIndent(self, indent, indent_size = 4, indent_char = ' '):
+		return (indent_char * indent_size) * indent
+
+	def renderLines(self, item, indent = 4, indent_char = ' ', indent_index = 0):
+		item_class = item.__class__
+		isRoot = item_class == ApacheRoot
+		output_lines = []
+		if isRoot or item_class == ApacheSection:
+			if not isRoot:
+				output_lines.append(self.renderIndent(indent_index, indent, indent_char) + str(item))
+			else:
+				indent_index -= 1
+			for child in item.children:
+				output_lines += self.renderLines(child, indent, indent_char, indent_index + 1)
+			if not isRoot:
+				output_lines.append(self.renderIndent(indent_index, indent, indent_char) + item.renderEndOfSection())
+		elif item.__class__ in [ApacheStatement, ApacheComment, ApacheEmptyLine]:
+			output_lines.append(self.renderIndent(indent_index, indent, indent_char) + str(item))
+		return output_lines
+
+	def render(self, item = None, indent = 4, indent_char = ' '):
+		item = self if not item else item
+		return "\n".join(self.renderLines(item, indent, indent_char)).encode('utf-8')
 
 	def renderEndOfSection(self):
 		return '</%s>' % (self.name,)
 
 	def __str__(self):
-		quoted_args = [shlex.quote(x) for x in self.arguments]
+		quoted_args = [shlex.quote(x) for x in self.arguments] if self.arguments else []
 		return '<%s %s>' % (self.name, ' '.join(quoted_args))
 
 	def __repr__(self):
-		return "<%s '%s' @ Line %d>" % (self.__class__.__name__, self.name, self.index)
+		return "<%s '%s' @ Line %d>" % (self.__class__.__name__, self.name, self.index if self.index != None else -1)
 
 class ApacheRoot(ApacheSection):
 	def __repr__(self):
 		return "<%s>" % (self.__class__.__name__,)
+
+def configBuilder(config, parent = None):
+	parent = ApacheRoot() if parent == None else parent
+	print(config['type'])
+	if config['type'] == 'root' and parent.__class__ == ApacheRoot:
+		for child in config['children']:
+			configBuilder(child, parent)
+	elif config['type'] == 'section':
+		new_section = ApacheSection()
+		new_section.name = config['name']
+		new_section.update(*config['args'])
+		for child in config['children']:
+			configBuilder(child, new_section)
+		parent.appendChild(new_section)
+	elif config['type'] == 'statement':
+		new_statement = ApacheStatement()
+		new_statement.module = config['module']
+		new_statement.update(*config['args'])
+		parent.appendChild(new_statement)
+	elif config['type'] == 'comment':
+		new_comment = ApacheComment()
+		new_comment.comment = config['comment']
+		parent.appendChild(new_comment)
+	elif config['type'] == 'emptyline':
+		parent.appendChild(ApacheEmptyLine())
+	else:
+		raise ValueError('Invalid Type %s' % (config['type'],))
+	return parent
 
 class ApacheParser:
 	def __init__(self, file, indent=4):
@@ -231,38 +291,24 @@ class ApacheParser:
 			line = self.file.readline()
 		return self.root
 
+	def appendChild(self, i):
+		return self.root.appendChild(i)
+
+	def insertBefore(self, n, o):
+		return self.root.insertBefore(n, o)
+
 	def find(self, name, *arguments):
 		return self.root.find(name, *arguments)
 
 	def findAll(self, name, *arguments):
 		return self.root.findAll(name, *arguments)
 
+	def render(self, item = None, indent = 4, indent_char = ' '):
+		item = self.root if not item else item
+		return self.root.render(item, indent = indent, indent_char = indent_char)
+
 	def parse(self):
 		return self.parseFile()
-
-	def renderIndent(self, indent):
-		return (' ' * self.indent_size) * indent
-
-	def renderLines(self, item, indent = 0):
-		item_class = item.__class__
-		isRoot = item_class == ApacheRoot
-		output_lines = []
-		if isRoot or item_class == ApacheSection:
-			if not isRoot:
-				output_lines.append(self.renderIndent(indent) + str(item))
-			else:
-				indent -= 1
-			for child in item.children:
-				output_lines += self.renderLines(child, indent + 1)
-			if not isRoot:
-				output_lines.append(self.renderIndent(indent) + item.renderEndOfSection())
-		elif item.__class__ in [ApacheStatement, ApacheComment, ApacheEmptyLine]:
-			output_lines.append(self.renderIndent(indent) + str(item))
-		return output_lines
-
-	def render(self, item = None):
-		item = self.root if not item else item
-		return "\n".join(self.renderLines(item)).encode('utf-8')
 
 if __name__ == '__main__':
 	import sys
